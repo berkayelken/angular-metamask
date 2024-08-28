528e933fd05ef83f9caa625fa8f96b303d6ba6e7
Berkay Yelken <berkayelken@gmail.com>
2024-08-28 10:16:49 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-metamask';
+  minniesNft = 0
+
+  public signInWithMetaMask() {
+    detectEthereumProvider().then(provider => {
+        if (!provider) {
+          throw new Error('Please install MetaMask');
+        }
+        let ethereum: any = provider;
+        let nftAddr = "0xc0567ECFf3b11F5Ec995Ff7c9Df23087B6096238"
+        let nft: Promise<any> = ethereum.request({ method: "eth_getBalance", params: [nftAddr]})
+        nft.then(res =>  this.minniesNft = Number(res))
+    })
+  }
 }
